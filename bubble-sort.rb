4502a472f6a2c0fc468fde39ed66a6a818053fb6
abmasadullah <abmasadullah@gmail.com>
2020-09-30 03:00:20 +0600

    Algorithm modified

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -9,18 +9,16 @@ array_test = [2,4,1,7,5]
 
 # Buble sort by
 def bubble_sort_by(array)
-    j = 1
-    while j < array.length
-        i = j
-        k = 0
-        while i < (array.length - k)
-            if array[i] < array[i-1]
-                temp = array[i-1]
-                array[i-1] = array[i]
-                array[i] = temp        
+    j = 0
+    while j < array.length - 1
+        i = 0
+        while i < array.length - (j + 1)
+            if array[i+1] < array[i]
+                temp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = temp        
             end
             i += 1
-            k += 1
         end
         j += 1
     end
